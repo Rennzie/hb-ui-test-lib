70c70b49609804e4747236bde67710c4c3773a60
Rennzie <sean@hummingbirdtech.com>
2020-02-07 15:03:51 +0000

    add: code formatting

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 
 export interface IProps {
   children: React.ReactChild;
   /** The button size */
-  size: "small" | "large";
+  size: 'small' | 'large';
   margin: any;
 }
 
@@ -12,11 +12,11 @@ function Button({ children, size, margin = 12 }: IProps) {
     <button
       style={{
         margin,
-        border: "none",
-        padding: size === "small" ? "8px 12px" : "12px 16px",
-        background: "hotpink",
-        borderRadius: "4px",
-        color: "white"
+        border: 'none',
+        padding: size === 'small' ? '8px 12px' : '12px 16px',
+        background: 'hotpink',
+        borderRadius: '4px',
+        color: 'white',
       }}
     >
       {children}
--- styleguide.config.js
+++ styleguide.config.js
@@ -1,6 +1,7 @@
+/*eslint-disable*/
 module.exports = {
-  components: "src/components/**/*.tsx",
-  propsParser: require("react-docgen-typescript").withCustomConfig(
-    "./tsconfig.json"
-  ).parse
+  components: 'src/components/**/*.tsx',
+  propsParser: require('react-docgen-typescript').withCustomConfig(
+    './tsconfig.json',
+  ).parse,
 };
